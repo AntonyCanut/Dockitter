0851ee5957f1eb2e94752e2ea2c8db729821fd4f
Antony Canut <canut42@gmail.com>
2016-05-11 09:04:13 +0200

    handlebars like OK

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  console.log(viewModel)
   viewModel.tweet = true;
-  var html = jade.render($("#panel-card-div").text(), {viewModel: viewModel})
-  console.log(html);
+  var html = jade.render($("#panel-card-div").text())
   $("#container-card").html(html);
 });

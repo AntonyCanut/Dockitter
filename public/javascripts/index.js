2cd268f1a02f9aa05ff734b9cffdb9559fe642d8
Antony Canut <canut42@gmail.com>
2016-05-11 03:05:20 +0200

    Modification de bower, ajout de jade side client / handlebars like

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  // String.prototype.replaceAll = function(str1, str2, ignore) {
-  //   return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-  // }
-  // viewModel = viewModel.replaceAll('\n', '');
-  // viewModel = JSON.parse(viewModel);
+  console.log(viewModel)
+  viewModel.tweet = true;
+  var html = jade.render($("#panel-card-div").text(), {viewModel: viewModel})
+  console.log(html);
+  $("#container-card").html(html);
 });
